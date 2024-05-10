8fe4f63ccbe83856f2dc89ce626f06a226fd08ef
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-05-10 12:24:21 -0600

    refactor: Change file to make connection with MondoDB complemented with state data

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -1,12 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost/test');
-        console.log('Successfully connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-};
+  const dbUri = 'mongodb://admin:1234@database:27017/therapisttrack'
+  try {
+    await mongoose.connect(dbUri)
+    console.log('Successfully connected to MongoDB')
 
-module.exports = connectDB;
+    const { readyState } = mongoose.connection
+    if (readyState === 1) {
+      console.log('MongoDB is connected')
+    } else {
+      console.error('Failed to connect to MongoDB: State is', readyState)
+      process.exit(1)
+    }
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message)
+    process.exit(1)
+  }
+}
+
+process.on('SIGINT', async () => {
+  console.log('Closing MongoDB connection')
+  await mongoose.connection.close()
+  console.log('MongoDB connection closed')
+  process.exit(0)
+})
+
+module.exports = connectDB
