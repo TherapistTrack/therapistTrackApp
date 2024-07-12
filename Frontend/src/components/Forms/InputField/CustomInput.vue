91180e9a7dd50de0d9b240b269ead9f2cfd45ae3
DanielRasho <ray22933@uvg.edu.gt>
2024-07-11 23:56:20 -0600

    refactor: change project structure for components and add more components categories

---
+++ Frontend/src/components/Forms/InputField/CustomInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="customInput">
+    <input :placeholder="pholder" /><img src="../assets/search-line.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pholder: String
+  }
+}
+</script>
+
+<style>
+.customInput {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  background-color: #e9e9e9;
+  border: 0;
+  border-radius: 2vh;
+  padding: 1vh;
+  color: #6e6e6e;
+}
+
+.customInput input {
+  border: none;
+  background: none;
+}
+
+.customInput img {
+  height: 3vh;
+}
+</style>
